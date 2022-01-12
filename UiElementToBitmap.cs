91726cbdee15040014756d1c3f9bce5627f20c12
Nick Schumer <nick.schumer@impower.ai>
2022-01-12 14:48:07 -0500

    Repo init

---
+++ UiElementToBitmap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+using System.Drawing;
+using UiPath.Core;
+using UiPath.Activities;
+using System.IO;
+
+namespace Impower.UiAutomation
+{
+    public class UiElementToBitmap : CodeActivity
+    {
+        [RequiredArgument]
+        public InArgument<UiElement> InputElement { get; set; }
+        public OutArgument<Bitmap> OutputBitmap { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            UiElement inputEl = InputElement.Get(context);
+            UiPath.Core.Image screenshot = inputEl.Screenshot();
+            byte[] screenshotByteArr = screenshot.ByteArray;
+            Bitmap bmp;
+            using (var ms = new MemoryStream(screenshotByteArr))
+            {
+                bmp = new Bitmap(ms);
+            }
+            OutputBitmap.Set(context, bmp);
+        }
+    }
+}
